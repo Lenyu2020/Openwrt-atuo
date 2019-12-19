d8da34b8a2756ceee3891d128724edeb287899d1
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2019-12-19 20:02:48 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,4 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.14/g' /home/perfume/lede/target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.14/g' openwrt/target/linux/x86/Makefile
