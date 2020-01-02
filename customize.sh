e01a8d058f7410dc8314f8037fee88e495499a37
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2020-01-02 17:16:48 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,10 +8,4 @@
 #1. Modify default IP
   sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
   sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.14/g' openwrt/target/linux/x86/Makefile
-#Aad Lienol feeds
-#使用echo命令添加
-  #echo "src-git lienol https://github.com/Lienol/openwrt-package" >> openwrt/feeds.conf.default
-#sed命令也是可行的
-# sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' openwrt/feeds.conf.default
-#Update & Install feeds
-  #cd openwrt && ./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+  sed -i 's/R9.12.31/R9.12.31_Len yu/g'  openwrt/package/lean/default-settings/files/zzz-default-settings
