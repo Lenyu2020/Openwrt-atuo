969bd579d091d251c1008ac3b2afc5ef4f962421
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2020-01-11 18:08:25 +0800

    Create update_v2ray.sh

---
+++ update_v2ray.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+cd /tmp/tmp
+wget https://github.com/v2ray/v2ray-core/releases/download/v4.22.1/v2ray-linux-64.zip
+unzip -o v2ray-linux-64.zip
+mv -f /tmp/tmp/v2ctl  /usr/bin/v2ray
+mv -f /tmp/tmp/v2ray  /usr/bin/v2ray
+chmod +x /usr/bin/v2ray/v2ctl
+chmod +x /usr/bin/v2ray/v2ray
+echo 请到路由后台程序保存应用一下XXSSRplus+
+sleep 5
+exit 0
